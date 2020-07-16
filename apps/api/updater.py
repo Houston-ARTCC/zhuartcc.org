4e8a34d7e7fc41ca164c1cdcb8c0f97a59190513
Michael Romashov <mromashov@icloud.com>
2020-07-16 15:17:49 -0400

    Automatic activity warning emails.

--- apps/api/updater.py
+++ apps/api/updater.py
@@ -1,12 +1,19 @@
+import calendar
 from datetime import datetime
 
 import pytz
+from django.core.mail import send_mail
+from django.db.models import Sum, Q
+from django.template.loader import render_to_string
 from django.utils import timezone
 
 import requests
 from apscheduler.schedulers.background import BackgroundScheduler
+from django.utils.html import strip_tags
 
 from zhuartcc import settings
+
+from .views import return_hour_aggregate
 from ..api.models import Controller, ControllerSession
 from ..user.models import User
 
@@ -15,6 +22,7 @@ def update_scheduler():
     pull_controllers()
     scheduler = BackgroundScheduler()
     scheduler.add_job(pull_controllers, 'interval', minutes=1)
+    scheduler.add_job(warn_inactive_users, 'cron', day=23)
     scheduler.start()
 
 
@@ -49,3 +57,31 @@ def pull_controllers():
                             online_since=pytz.utc.localize(datetime.strptime(controller[36], '%Y%m%d%H%M%S')),
                             last_update=timezone.now(),
                         ).save()
+
+
+def return_inactive_users():
+    inactive_users = []
+    for user_status in [return_hour_aggregate(user) for user in User.objects.exclude(main_role='MC')]:
+        if 'current_status' in user_status and not user_status['current_status']:
+            inactive_users.append(user_status['user_obj'])
+
+    return inactive_users
+
+
+def warn_inactive_users():
+    for user in return_inactive_users():
+        session_aggregate = ControllerSession.objects.filter(user=user).aggregate(
+            current=Sum('duration', filter=Q(time_logon__month=timezone.now().month))
+        )
+        context = {
+            'user': user,
+            'hours': round(session_aggregate['current'].total_seconds() / 3600, 1),
+            'deadline': f'{calendar.month_name[timezone.now().month + 1]} 5'
+        }
+        send_mail(
+            'Controller Activity Warning',
+            render_to_string('emails/activity_warning.txt', context),
+            'no-reply@zhuartcc.org',
+            [user.email],
+            html_message=render_to_string('emails/activity_warning.html', context),
+        )

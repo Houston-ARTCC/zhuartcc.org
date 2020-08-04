dcc6615a63032fe9705ce04c40da9abcacfb1283
Michael Romashov <mromashov@icloud.com>
2020-08-04 18:34:01 -0400

    Created training models.

--- apps/training/admin.py
+++ apps/training/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import TrainingSession
+from .models import TrainingSession, Exam, ExamQuestion
 
 admin.site.register(TrainingSession)
+admin.site.register(Exam)
+admin.site.register(ExamQuestion)
--- apps/training/models.py
+++ apps/training/models.py
@@ -1,51 +1,48 @@
-import datetime
+from datetime import timedelta
 from django.db import models
 from ..user.models import User
 
 
 class TrainingSession(models.Model):
-    student = models.ForeignKey(User, models.SET_NULL, null=True, related_name='student_sessions')
-    instructor = models.ForeignKey(User, models.SET_NULL, null=True, related_name='instructor_sessions')
+    TYPES = (
+        (0, 'Classroom'),
+        (1, 'Sweatbox'),
+        (2, 'Online'),
+        (3, 'OTS'),
+    )
+    STATUSES = (
+        (0, 'Scheduled'),
+        (1, 'Completed'),
+        (2, 'Cancelled'),
+        (3, 'No-Show'),
+    )
+    student = models.ForeignKey(User, models.SET_NULL, null=True, blank=True, related_name='student_sessions')
+    instructor = models.ForeignKey(User, models.SET_NULL, null=True, blank=True, related_name='instructor_sessions')
     start = models.DateTimeField()
-    duration = models.TimeField(default=datetime.time(hour=1))
-    position = models.CharField(max_length=16, null=True)
-    level = models.IntegerField()
-    type = models.IntegerField()
-    status = models.IntegerField()
-    session_notes = models.FileField(upload_to='training/', null=True)
-    ots_rec = models.BooleanField(default=False)
-    ots_passed = models.BooleanField(default=False)
+    duration = models.DurationField(default=timedelta(hours=1))
+    position = models.CharField(max_length=16, null=True, blank=True)
+    type = models.IntegerField(choices=TYPES)
+    status = models.IntegerField(default=0, choices=STATUSES)
+    session_notes = models.FileField(upload_to='training/', null=True, blank=True)
 
-    # Returns string representation of session type
-    def return_type_str(self):
-        if self.type == 0:
-            return 'Classroom'
-        elif self.type == 1:
-            return 'Sweatbox'
-        elif self.type == 2:
-            return 'Online'
-        elif self.type == 3:
-            return 'OTS'
+    def __str__(self):
+        return f'{self.start} | {self.student.return_full_name()} with {self.instructor.return_full_name()}'
 
-    # Returns string representation of the session level
-    def return_level_str(self):
-        if self.level == 0:
-            return 'Minor Delivery'
-        elif self.level == 1:
-            return 'Major Delivery'
-        elif self.level == 2:
-            return 'Minor Ground'
-        elif self.level == 3:
-            return 'Major Ground'
-        elif self.level == 4:
-            return 'Minor Tower'
-        elif self.level == 5:
-            return 'Major Tower'
-        elif self.level == 6:
-            return 'Minor Approach'
-        elif self.level == 7:
-            return 'Major Approach'
-        elif self.level == 8:
-            return 'Center'
-        elif self.level == 9:
-            return 'Oceanic'
+
+class ExamQuestion(models.Model):
+    question = models.TextField()
+
+    def __str__(self):
+        return self.question
+
+
+class Exam(models.Model):
+    student = models.ForeignKey(User, models.CASCADE, related_name='student_exams')
+    name = models.CharField(max_length=255)
+    questions = models.ManyToManyField(ExamQuestion)
+    assigned = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField()
+    score = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.assigned} | {self.student.return_full_name()} - {self.name}'

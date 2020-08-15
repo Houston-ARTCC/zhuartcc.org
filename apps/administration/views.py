5777394834539b5af5029d36e16f15780e1dff8e
Michael Romashov <mromashov@icloud.com>
2020-08-15 15:36:53 -0400

    Added transfer requests from VATUSA.

--- apps/administration/views.py
+++ apps/administration/views.py
@@ -9,3 +9,12 @@ from zhuartcc.decorators import require_staff
 @require_staff
 def view_admin_panel(request):
     return render(request, 'admin_panel.html', {'page_title': 'Admin Panel'})
+
+
+@require_staff
+def view_transfers(request):
+    transfers = requests.get(
+        'https://api.vatusa.net/v2/facility/ZHU/transfers',
+        params={'apikey': settings.API_KEY},
+    ).json()['transfers']
+    return render(request, 'transfers.html', {'page_title': 'Transfers', 'transfers': transfers})

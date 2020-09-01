b48e7d5fbdcc2beaf96f26427bf66f77b881d304
Michael Romashov <mromashov@icloud.com>
2020-09-01 17:50:28 -0400

    Added training request model.

--- apps/training/models.py
+++ apps/training/models.py
@@ -4,23 +4,25 @@ from django.db import models
 from ..user.models import User
 
 
+TYPES = (
+    (0, 'Classroom'),
+    (1, 'Sweatbox'),
+    (2, 'Online'),
+    (3, 'OTS'),
+)
+LEVELS = (
+    (0, 'Minor Ground'),
+    (1, 'Major Ground'),
+    (2, 'Minor Tower'),
+    (3, 'Major Tower'),
+    (4, 'Minor Approach'),
+    (5, 'Major Approach'),
+    (6, 'Center'),
+    (7, 'Oceanic'),
+)
+
+
 class TrainingSession(models.Model):
-    TYPES = (
-        (0, 'Classroom'),
-        (1, 'Sweatbox'),
-        (2, 'Online'),
-        (3, 'OTS'),
-    )
-    LEVELS = (
-        (0, 'Minor Ground'),
-        (1, 'Major Ground'),
-        (2, 'Minor Tower'),
-        (3, 'Major Tower'),
-        (4, 'Minor Approach'),
-        (5, 'Major Approach'),
-        (6, 'Center'),
-        (7, 'Oceanic'),
-    )
     STATUSES = (
         (0, 'Scheduled'),
         (1, 'Completed'),
@@ -39,3 +41,14 @@ class TrainingSession(models.Model):
 
     def __str__(self):
         return f'{self.start} | {self.student.full_name} with {self.instructor.full_name}'
+
+
+class TrainingRequest(models.Model):
+    student = models.ForeignKey(User, models.CASCADE, related_name='training_requests')
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    type = models.IntegerField(choices=TYPES)
+    level = models.IntegerField(choices=LEVELS)
+
+    def __str__(self):
+        return f'{self.student.full_name} | Training request for {self.get_level_display()}'

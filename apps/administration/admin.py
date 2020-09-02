fe535fd702cd0a9458642553bbf00e8b261239be
Michael Romashov <mromashov@icloud.com>
2020-09-01 20:54:36 -0400

    Added announcement model.

--- apps/administration/admin.py
+++ apps/administration/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import ActionLog
+from .models import ActionLog, Announcement
 
 admin.site.register(ActionLog)
+admin.site.register(Announcement)
--- apps/administration/models.py
+++ apps/administration/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
+from apps.user.models import User
+
 
 class ActionLog(models.Model):
     action = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Announcement(models.Model):
+    author = models.ForeignKey(User, models.SET_NULL, null=True, blank=True, related_name='announcements')
+    subject = models.CharField(max_length=64)
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)

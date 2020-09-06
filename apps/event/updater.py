9f2dbfd63a19315e7b12b6857bed237034702c87
Michael Romashov <mromashov@icloud.com>
2020-09-06 15:35:33 -0400

    Fixed event score calculation.

--- apps/event/updater.py
+++ apps/event/updater.py
@@ -14,7 +14,7 @@ def start():
 
 
 def update_event_scores():
-    past_events = Event.objects.filter(end__gte=timezone.now() - timedelta(hours=1))
+    past_events = Event.objects.filter(end__lte=timezone.now()).filter(end__gte=timezone.now() - timedelta(hours=1))
     if past_events.exists():
         for event in past_events:
             event.calculate_scores()

2fa58e543efd61b997a94e73ab590fbb3a540d2b
Michael Romashov <mromashov@icloud.com>
2020-09-06 19:43:54 -0400

    MAVP controller login.

--- apps/uls/views.py
+++ apps/uls/views.py
@@ -1,18 +1,23 @@
+import ast
 import os
 import hmac
 import requests
 from base64 import urlsafe_b64encode, urlsafe_b64decode
 
+from django.http import HttpResponseServerError
 from django.shortcuts import redirect
 from django.urls import reverse
 
+from ..administration.models import ActionLog
 from ..user.models import User
+from ..user.updater import assign_oper_init
 
 
 def login(request):
+    print(request.headers)
     # Checks if user has a token from VATUSA
     if request.GET.get('token'):
-        raw_token = request.GET['token']
+        raw_token = request.GET.get('token')
         token = raw_token.split('.')
 
         token_sig = token[2]
@@ -26,21 +31,40 @@ def login(request):
 
         if token_sig == jwk_sig:
             data = requests.get(f'https://login.vatusa.net/uls/v2/info?token={token[1]}').json()
-            
+
             request.session['vatsim_data'] = data
+            request.session['cid'] = data['cid']
 
             user_query = User.objects.filter(cid=data['cid'])
-            request.session['cid'] = data['cid']
 
             if user_query.exists() and user_query[0].status != 2:
                 user = User.objects.get(cid=data['cid'])
                 request.session['guest'] = False
                 request.session['staff'] = user.is_staff
                 request.session['mentor'] = user.is_mentor
+            elif data['facility']['id'] in ast.literal_eval(os.getenv('MAVP_ARTCCS')):
+                new_user = User(
+                    first_name=data['fname'].capitalize(),
+                    last_name=data['lname'].capitalize(),
+                    cid=int(data['cid']),
+                    email=data['email'],
+                    oper_init=assign_oper_init(data['fname'][0], data['lname'][0]),
+                    rating=data['rating_short'],
+                    main_role='MC',
+                )
+                new_user.save()
+                new_user.assign_initial_cert()
+                ActionLog(action=f'User {new_user.full_name} was created by system.').save()
+
+                request.session['guest'] = False
+                request.session['staff'] = False
+                request.session['mentor'] = False
             else:
                 request.session['guest'] = True
                 request.session['staff'] = False
                 request.session['mentor'] = False
+        else:
+            return HttpResponseServerError()
 
     return redirect(reverse('home'))
 

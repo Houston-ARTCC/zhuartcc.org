de72c9e9af8c883944bf1e7eb68495bd6a0355b4
Michael Romashov <mromashov@icloud.com>
2020-09-06 21:42:29 -0400

    Hidden events don't show on feedback form.

--- apps/feedback/views.py
+++ apps/feedback/views.py
@@ -34,5 +34,5 @@ def add_feedback(request):
         return render(request, 'add_feedback.html', {
             'page_title': 'Submit Feedback',
             'controllers': User.objects.filter(status=0),
-            'events': Event.objects.filter(start__gte=timezone.now() - timedelta(days=30)),
+            'events': Event.objects.filter(start__gte=timezone.now() - timedelta(days=30)).filter(hidden=False),
         })

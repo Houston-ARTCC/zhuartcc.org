aa0e90342da6aa2c308cfbfdb99748e6f797d854
Michael Romashov <mromashov@icloud.com>
2020-09-08 22:10:40 -0400

    Cleaned up view decorators.

--- zhuartcc/decorators.py
+++ zhuartcc/decorators.py
@@ -2,54 +2,41 @@ from functools import wraps
 from django.http import HttpResponse
 
 
-# View decorator that checks session to see if user is staff
 def require_staff(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
-        if 'vatsim_data' in request.session:
-            if 'staff' in request.session and request.session['staff']:
-                return function(request, *args, **kwargs)
-            else:
-                return HttpResponse('You must be a staff member to complete this action!', status=401)
+        if request.session.get('staff'):
+            return function(request, *args, **kwargs)
         else:
-            return HttpResponse('You must be logged in to complete this action!', status=401)
+            return HttpResponse('You must be a staff member to access this endpoint!', status=403)
     return wrap
 
 
-# View decorator that checks session to see if user is a mentor
 def require_mentor(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
-        if 'vatsim_data' in request.session:
-            if 'mentor' in request.session and request.session['mentor']:
-                return function(request, *args, **kwargs)
-            else:
-                return HttpResponse('You must be a training staff member to complete this action!', status=401)
+        if request.session.get('mentor'):
+            return function(request, *args, **kwargs)
         else:
-            return HttpResponse('You must be logged in to complete this action!', status=401)
+            return HttpResponse('You must be a mentor or instructor to access this endpoint!', status=403)
     return wrap
 
 
-# View decorator that checks session to see if user is logged in
 def require_logged_in(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
-        if 'vatsim_data' in request.session:
+        if request.session.get('vatsim-data'):
             return function(request, *args, **kwargs)
         else:
-            return HttpResponse('You must be logged in to complete this action!', status=401)
+            return HttpResponse('You must be logged in to access this endpoint!', status=403)
     return wrap
 
 
-# View decorator that checks session to see if user is a member of the ARTCC
 def require_member(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
-        if 'vatsim_data' in request.session:
-            if 'guest' in request.session and not request.session['guest']:
-                return function(request, *args, **kwargs)
-            else:
-                return HttpResponse('You must be a member to complete this action!', status=401)
+        if not request.session.get('guest'):
+            return function(request, *args, **kwargs)
         else:
-            return HttpResponse('You must be logged in to complete this action!', status=401)
+            return HttpResponse('You must be an active Houston controller to access this endpoint!', status=403)
     return wrap
